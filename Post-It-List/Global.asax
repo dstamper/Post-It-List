624699f0dcb6455fa715459c7485ba52b7e97c84
ahunger603 <ahunger@pdx.edu>
2017-07-07 21:38:13 -0700

    template files

---
+++ Post-It-List/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Post_It_List.WebApiApplication" Language="C#" %>
---
+++ Post-It-List/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Post_It_List
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
