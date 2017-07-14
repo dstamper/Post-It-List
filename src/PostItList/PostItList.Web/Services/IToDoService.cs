098685c766c9cd958adff1d285f109443521e5db
David Stamper <dstamper@pdx.edu>
2017-07-13 20:11:18 -0700

    Moved the javascript front end to a new View

--- src/PostItList/PostItList.Web/Services/IToDoService.cs
+++ src/PostItList/PostItList.Web/Services/IToDoService.cs
@@ -8,7 +8,12 @@ namespace PostItList.Web.Services
 {
     public interface IToDoService
     {
+        //todo migrate to ID instead of title
         Task<bool> Add(ToDoItem item);
         Task<IEnumerable<ToDoItem>> GetAll();
+        Task<bool> Edit(ToDoItem item);
+        Task<bool> Delete(ToDoItem item);
+        Task<ToDoItem> Get(string title);
+        
     }
 }
